9690e166206584d02bc5f59ceff8d07be4476761
Daniel Tull <dt@danieltull.co.uk>
2019-06-22 22:58:12 +0100

    Remove use of UIKitForMac to see if Linux-based testing will work

--- Tests/ResourcedTests/ResourceCombineTests.swift
+++ Tests/ResourcedTests/ResourceCombineTests.swift
@@ -8,7 +8,6 @@ import Combine
 
 @available(iOS 13.0, *)
 @available(OSX 10.15, *)
-@available(UIKitForMac 13.0, *)
 @available(tvOS 13.0, *)
 @available(watchOS 6.0, *)
 final class ResourceCombineTests: XCTestCase {
