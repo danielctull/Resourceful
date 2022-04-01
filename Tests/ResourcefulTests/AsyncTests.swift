39d5c47d797214d4fec7e7aaebb848c578653eef
Daniel Tull <dt@danieltull.co.uk>
2022-04-01 13:19:15 +0100

    Fix deprecation warning

--- Tests/ResourcefulTests/AsyncTests.swift
+++ Tests/ResourcefulTests/AsyncTests.swift
@@ -11,12 +11,12 @@ import XCTest
 @available(watchOS 8.0, *)
 final class AsyncTests: XCTestCase {
 
-    func testSuccess() throws {
+    func testSuccess() async throws {
         // Linux doesn't support file-based URLs that this test uses.
         #if !os(Linux)
         try createFile(withContents: "Hello") { url in
             expect { completion in
-                detach {
+                Task.detached {
                     let value = try await URLSession.shared.value(for: self.resource(url))
                     XCTAssertEqual(value, "Hello")
                     completion()
@@ -30,7 +30,7 @@ final class AsyncTests: XCTestCase {
         // Linux doesn't support file-based URLs that this test uses.
         #if !os(Linux)
         expect { completion in
-            detach {
+            Task.detached {
                 do {
                     _ = try await URLSession.shared.value(for: self.failingRequestResource)
                     XCTFail("Should cause an error.")
@@ -47,7 +47,7 @@ final class AsyncTests: XCTestCase {
         #if !os(Linux)
         try createFile(withContents: "Hello") { base in
             expect { completion in
-                detach {
+                Task.detached {
                     do {
                         let url = base.appendingPathComponent("ThisDoesNotExist")
                         _ = try await URLSession.shared.value(for: self.resource(url))
