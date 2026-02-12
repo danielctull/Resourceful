9a7cb1ba0d3a97a32efb68cae193f6dccffc9bdf
Daniel Tull <dt@danieltull.co.uk>
2026-02-12 12:16:31 +0000

    Remove fetch code

--- Sources/Resourceful/Resource+Async.swift
+++ Sources/Resourceful/Resource+Async.swift
@@ -1,26 +1,20 @@
 import Foundation
 
 #if canImport(FoundationNetworking)
-  import FoundationNetworking
+import FoundationNetworking
 #endif
 
-#if swift(>=5.5)
+@available(iOS 13.0, *)
+@available(OSX 10.15, *)
+@available(tvOS 13.0, *)
+@available(watchOS 6.0, *)
+extension URLSession {
 
-  @available(iOS 13.0, *)
-  @available(OSX 10.15, *)
-  @available(tvOS 13.0, *)
-  @available(watchOS 6.0, *)
-  extension URLSession {
-
-    /// Asynchronously retrieves the value of the resource.
-    ///
-    /// - Parameter resource: The resource to fetch.
-    /// - Returns: The value of the resource.
-    public func value<Value>(for resource: Resource<Value>) async throws -> Value {
-      try await withUnsafeThrowingContinuation { continuation in
-        fetch(resource, completion: continuation.resume)
-      }
-    }
+  /// Asynchronously retrieves the value of the resource.
+  ///
+  /// - Parameter resource: The resource to fetch.
+  /// - Returns: The value of the resource.
+  public func value<Value>(for resource: Resource<Value>) async throws -> Value {
+    try await resource.value(for: data(for: resource.request))
   }
-
-#endif
+}
